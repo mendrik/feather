b553c646634758f9270acc5e8a831f1b3e700fea
Andreas Prudzilko <mendrik76@gmail.com>
2017-05-03 20:04:19 +0300

    fix refs

--- src/javascripts/typescript.js
+++ src/javascripts/typescript.js
@@ -1,9 +1,9 @@
-__extends = function (d, b) {
+const __extends = function (d, b) {
   function __() { this.constructor = d; }
   d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 
-__assign = Object.assign || function (t) {
+const __assign = Object.assign || function (t) {
       for (var s, i = 1, n = arguments.length; i < n; i++) {
         s = arguments[i];
         for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
@@ -11,7 +11,7 @@ __assign = Object.assign || function (t) {
       return t;
     };
 
-__rest = function (s, e) {
+const __rest = function (s, e) {
   var t = {};
   for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
     t[p] = s[p];
@@ -21,7 +21,7 @@ __rest = function (s, e) {
   return t;
 };
 
-__decorate = function (decorators, target, key, desc) {
+const __decorate = function (decorators, target, key, desc) {
   var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
   if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
   else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
