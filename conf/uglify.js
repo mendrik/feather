1bad19dd7c8a11703b899971f03d60377767301d
Andreas Prudzilko <mendrik76@gmail.com>
2017-05-21 10:32:41 +0300

    remove sourcemaps from distribution build

--- conf/uglify.js
+++ conf/uglify.js
@@ -6,12 +6,7 @@ let feather = fs.readFileSync('./out/javascripts/feather.js', 'utf-8')
 let result = u.minify({feather: feather}, {
     output: {
         preamble: fs.readFileSync("./out/javascripts/libs.min.js", "utf8")
-    },
-    sourceMap: {
-      root: '/feather',
-      filename: './out/javascripts/feather.js.map',
-      url: 'feather.min.map'
-    },
+    }
 })
 
 fs.writeFileSync('./out/javascripts/feather.min.js', result.code, 'utf8')
