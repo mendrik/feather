1ef035f54fe86b8d418f4c0618e87c2c7d2472ec
Andreas Prudzilko <mendrik76@gmail.com>
2017-05-21 10:48:38 +0300

    add hook test

--- test/template.spec.ts
+++ test/template.spec.ts
@@ -3,18 +3,18 @@ import {expect} from 'chai'
 
 describe('Templates', () => {
 
-    let window, feather, document;
+    let window, featherTs, document;
     before(done => featherStart(w => (
         window = w,
-        feather = w.feather,
+        featherTs = w.feather,
         document = w.document
     ) && done()))
 
     describe('Self closing tags', () => {
 
         it('Regexp works', () => {
-            let r = feather.annotations.selfClosingTags,
-                o = feather.annotations.openTags
+            let r = featherTs.annotations.selfClosingTags,
+                o = featherTs.annotations.openTags
             expect('<span/><span/>'.replace(r, o)).to.be.equal('<span></span><span></span>')
             expect('<bla><span/><span/></bla>'.replace(r, o)).to.be.equal('<bla><span></span><span></span></bla>')
             expect('<bla> <span/> <br><p/> <span/> </bla>'.replace(r, o)).to.be.equal('<bla> <span></span> <br><p></p> <span></span> </bla>')
@@ -27,11 +27,35 @@ describe('Templates', () => {
                     <AttributeWidget id="aw1" text="{'a'+'b'}" bool="{true}" func="{this.printStuff}" number="{3}"/>
                     <AttributeWidget id="aw2" text={this.printStuff()} bool={false} func={this.printStuff} number={4}/>
                 `,
-                pt = feather.annotations.getPreparsedTemplate,
+                pt = featherTs.annotations.getPreparsedTemplate,
                 parsed = pt(str);
             expect(parsed.node.children.length).to.be.equal(2);
             expect(parsed.node.children[0].getAttribute('number')).to.be.equal('{3}');
             expect(parsed.node.children[1].getAttribute('number')).to.be.equal('{4}');
+        })
+
+        it('Tenplate parses hooks', () => {
+            let str = `
+                    <AttributeWidget {{hook1}} bla="{{hook2}}" class="bub {{hook3}}">in {{hook4}} text</AttributeWidget>
+                `,
+                pt = featherTs.annotations.getPreparsedTemplate,
+                parsed = pt(str)
+            expect(parsed.hookInfos.length).to.be.equal(4);
+            expect(parsed.hookInfos[0].curly).to.be.equal('hook1')
+            expect(parsed.hookInfos[0].text).to.be.undefined
+            expect(parsed.hookInfos[0].type).to.be.equal(feather.annotations.HookType.PROPERTY)
+
+            expect(parsed.hookInfos[1].curly).to.be.equal('hook2')
+            expect(parsed.hookInfos[1].text).to.be.equal('bla')
+            expect(parsed.hookInfos[1].type).to.be.equal(feather.annotations.HookType.ATTRIBUTE)
+
+            expect(parsed.hookInfos[2].curly).to.be.equal('hook3')
+            expect(parsed.hookInfos[2].text).to.be.undefined
+            expect(parsed.hookInfos[2].type).to.be.equal(feather.annotations.HookType.CLASS)
+
+            expect(parsed.hookInfos[3].curly).to.be.equal('hook4')
+            expect(parsed.hookInfos[3].text).to.be.equal('in {{hook4}} text')
+            expect(parsed.hookInfos[3].type).to.be.equal(feather.annotations.HookType.TEXT)
         })
     })
     
