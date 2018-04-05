d6e8f4705de552ac15acc6df9f732f279f7cc364
Andreas Prudzilko <mendrik76@gmail.com>
2018-04-05 17:08:55 +0300

    fix self closing regexp

--- src/typescript/feather/annotations/template.ts
+++ src/typescript/feather/annotations/template.ts
@@ -15,7 +15,7 @@ module feather.annotations {
     const ALL_CURLIES            = /{{(.*?)}}/g
     const templates              = new Map<any, TypedMap<Function>>()
     const parsedTemplateCache    = {} as Map<string, PreparsedTemplate>
-    export const selfClosingTags = /(<([^<>]+)(\s+[^<>]+(=[^<>]+|=["][^"]*["]|=['][^']*[']|={{?[^}]*?}?})?)*)\s*\/>/gmi
+    export const selfClosingTags = /(<([^<>\s]+)(\s+[^<>\s]+(=[\w\d]+|=["][^"]*["]|=['][^']*[']|={{?[^}]*?}?})?)*)\s*\/>/gmi
     export const openTags        = '$1></$2>'
 
     export enum HookType {
